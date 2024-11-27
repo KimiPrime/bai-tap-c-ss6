b2a70f07b894bd43f346d724cf37985d31d2ab54
KimiPrime <kingsan049@gmail.com>
2024-11-27 11:42:18 +0700

    Add files via upload

---
+++ bai3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int pass;
+	printf("vui long nhap vao mat khau: ");
+	scanf("%d",&pass);
+	if(pass==88888888){
+		printf("dung roi hacker a");
+	}else{
+		printf("sai roi pass dung la: 88888888");
+	}
+	return 0;
+}
